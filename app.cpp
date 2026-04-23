9f833663cb47977a61ab3ef4442b559b8ed1b41e
Lazy Phogat <maheshphogat550@gmail.com>
2026-04-23 13:36:18 +0530

    fixed missing main function

---
+++ app.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "DevOps Pipeline Running inside Docker..." << endl;
+    cout << "Container is active. Press Enter to exit..." << endl;
+    cin.get();
+    return 0;
+}
